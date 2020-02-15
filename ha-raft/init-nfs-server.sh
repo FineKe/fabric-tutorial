666199a514bb3402dcd0b4816c341a9ad233915a
fine <tianrandailoving@gmail.com>
2020-02-15 19:39:09 +0800

    ha-raft

---
+++ ha-raft/init-nfs-server.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+sudo su
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+sleep 3s
+yum install -y nfs-utils rpcbind
+chkconfig nfs on
+chkconfig rpcbind on
+service rpcbind start
+service nfs start
+mkdir /share
+
+echo "/share 192.168.33.11(rw,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "/share 192.168.33.12(rw,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "/share 192.168.33.13(rw,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "/share 192.168.33.21(rw,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "/share 192.168.33.22(rw,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "/share 192.168.33.23(rw,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "/share 192.168.33.24(rw,no_root_squash,no_subtree_check)" >> /etc/exports
+exportfs -a
